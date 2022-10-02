d4794ea06c751be9994e407dd0d64964126c958a
rahulmaurya-exe <rahulmaurya0001@gmail.com>
2022-10-02 20:23:00 +0530

    added sudoku generator and ani effects

--- static/js/app.js
+++ static/js/app.js
@@ -18,6 +18,8 @@ const pause_screen = document.querySelector('#pause-screen')
 const cells = document.querySelectorAll('.main-grid-cell')
 const name_input = document.querySelector('#input-name')
 
+const number_inputs = document.querySelectorAll('.number')
+
 const player_name = document.querySelector('#player-name')
 const game_level = document.querySelector('#game-level')
 const game_time = document.querySelector('#game-time')
@@ -28,6 +30,11 @@ let level = CONSTANT.LEVEL[level_index]
 let timer = null
 let pause = false
 let seconds = 0
+
+let se = undefined
+let su_answer = undefined
+
+let selected_cell = -1
 //-----------
 
 const getGameInfo = () => JSON.parse(localStorage.getItem('game'))
@@ -51,9 +58,210 @@ const getPlayerName = () => localStorage.getItem('player_name')
 const showTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substr(11, 8)
 
-const initSudoku = () =>{
-  //generate sudoku puzzle
+const clearSudoku = () => {
+  for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+    cells[i].innerHTML = ''
+    cells[i].classList.remove('filled')
+    cells[i].classList.remove('selected')
+  }
 }
+
+const initSudoku = () => {
+  //clear old sudoku
+  clearSudoku()
+  resetBg()
+
+  //generate sudoku puzzle
+  su = sudokuGen(level)
+  su_answer = [...su.question]
+
+  console.table(su_answer)
+
+  //show sudoku to div
+
+  for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+    let row = Math.floor(i / CONSTANT.GRID_SIZE)
+    let col = i % CONSTANT.GRID_SIZE
+
+    cells[i].setAttribute('data-value', su.question[row][col])
+
+    if (su.question[row][col] !== 0) {
+      cells[i].classList.add('filled')
+      cells[i].innerHTML = su.question[row][col]
+    }
+  }
+}
+
+const hoverBg = (index) => {
+  let row = Math.floor(index / CONSTANT.GRID_SIZE)
+  let col = index % CONSTANT.GRID_SIZE
+
+  let box_start_row = row - (row % 3)
+  let box_start_col = col - (col % 3)
+
+  for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+    for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+      let cell = cells[9 * (box_start_row + i) + (box_start_col + j)]
+      cell.classList.add('hover')
+    }
+  }
+
+  let step = 9
+  while (index - step >= 0) {
+    cells[index - step].classList.add('hover')
+    step += 9
+  }
+
+  step = 9
+  while (index + step < 81) {
+    cells[index + step].classList.add('hover')
+    step += 9
+  }
+
+  step = 1
+  while (index - step >= 9 * row) {
+    cells[index - step].classList.add('hover')
+    step += 1
+  }
+
+  step = 1
+  while (index + step < 9 * row + 9) {
+    cells[index + step].classList.add('hover')
+    step += 1
+  }
+}
+
+const resetBg = () => {
+  cells.forEach((e) => e.classList.remove('hover'))
+}
+
+const checkErr = (value) => {
+  const addErr = (cell) => {
+    if (parseInt(cell.getAttribute('data-value')) === value) {
+      cell.classList.add('err')
+      cell.classList.add('cell-err')
+      setTimeout(() => {
+        cell.classList.remove('cell-err')
+      }, 500)
+    }
+  }
+
+  let index = selected_cell
+
+  let row = Math.floor(index / CONSTANT.GRID_SIZE)
+  let col = index % CONSTANT.GRID_SIZE
+
+  let box_start_row = row - (row % 3)
+  let box_start_col = col - (col % 3)
+
+  for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+    for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+      let cell = cells[9 * (box_start_row + i) + (box_start_col + j)]
+      if (!cell.classList.contains('selected')) addErr(cell)
+    }
+  }
+
+  let step = 9
+  while (index - step >= 0) {
+    addErr(cells[index - step])
+    step += 9
+  }
+
+  step = 9
+  while (index + step < 81) {
+    addErr(cells[index + step])
+    step += 9
+  }
+
+  step = 1
+  while (index - step >= 9 * row) {
+    addErr(cells[index - step])
+    step += 1
+  }
+
+  step = 1
+  while (index + step < 9 * row + 9) {
+    addErr(cells[index + step])
+    step += 1
+  }
+}
+
+const removeErr = () => cells.forEach((e) => e.classList.remove('err'))
+
+const saveGameInfo = () => {
+  let game = {
+    level: level_index,
+    seconds: seconds,
+    su: {
+      original: su.original,
+      question: sq.question,
+      answer: su_answer,
+    },
+  }
+  localStorage.setItem('game', JSON.stringify(game))
+}
+
+const removeGameInfo = () => {
+  localStorage.removeItem('game')
+  document.querySelector('#btn-continue').style.display = 'none'
+}
+
+const isGameWin = () => {
+  sudokuCheck(su_answer)
+}
+
+const showResult = () => {
+  clearInterval(timer)
+  alert('win')
+}
+
+const initNumberInputEvent = () => {
+  number_inputs.forEach((e, index) => {
+    e.addEventListener('click', () => {
+      if (!cells[selected_cell].classList.contains('filled')) {
+        cells[selected_cell].innerHTML = index + 1
+        cells[selected_cell].setAttribute('data-value', index + 1)
+        // add to answer
+        let row = Math.floor(selected_cell / CONSTANT.GRID_SIZE)
+        let col = selected_cell % CONSTANT.GRID_SIZE
+        su_answer[row][col] = index + 1
+        // save game
+        // saveGameInfo()
+        // -----
+        removeErr()
+        checkErr(index + 1)
+        cells[selected_cell].classList.add('zoom-in')
+        setTimeout(() => {
+          cells[selected_cell].classList.remove('zoom-in')
+        }, 500)
+
+        // check game win
+        if (isGameWin()) {
+          removeGameInfo()
+          showResult()
+        }
+        // ----
+      }
+    })
+  })
+}
+
+const initCellsEvent = () => {
+  cells.forEach((e, index) => {
+    e.addEventListener('click', () => {
+      if (!e.classList.contains('filled')) {
+        cells.forEach((e) => e.classList.remove('selected'))
+
+        selected_cell = index
+        e.classList.remove('err')
+        e.classList.add('selected')
+        resetBg()
+        hoverBg(index)
+      }
+    })
+  })
+}
+
 const startGame = () => {
   start_screen.classList.remove('active')
   game_screen.classList.add('active')
@@ -64,6 +272,7 @@ const startGame = () => {
   game_level.innerHTML = CONSTANT.LEVEL_NAME[level_index]
 
   seconds = 0
+  showTime(seconds)
 
   timer = setInterval(() => {
     if (!pause) {
@@ -94,6 +303,7 @@ document.querySelector('#btn-level').addEventListener('click', (e) => {
 document.querySelector('#btn-play').addEventListener('click', () => {
   if (name_input.value.trim().length > 0) {
     console.log(name_input.value)
+    initSudoku()
     startGame()
   } else {
     name_input.classList.add('input-err')
@@ -117,6 +327,17 @@ document.querySelector('#btn-resume').addEventListener('click', () => {
 document.querySelector('#btn-new-game').addEventListener('click', () => {
   returnStartScreen()
 })
+
+document.querySelector('#btn-delete').addEventListener('click', () => {
+  cells[selected_cell].innerHTML = ''
+  cells[selected_cell].setAttribute('data-value', 0)
+
+  let row = Math.floor(selected_cell / CONSTANT.GRID_SIZE)
+  let col = selected_cell % CONSTANT.GRID_SIZE
+
+  su_answer[row][col] = 0
+  removeErr()
+})
 //----------------------------
 const init = () => {
   const darkmode = localStorage.getItem('darkmode')
@@ -128,7 +349,10 @@ const init = () => {
   const game = getGameInfo()
 
   document.querySelector('#btn-continue').style.display = game ? 'grid' : 'none'
+
   initGameGrid()
+  initCellsEvent()
+  initNumberInputEvent()
 
   if (getPlayerName()) {
     name_input.value = getPlayerName()
